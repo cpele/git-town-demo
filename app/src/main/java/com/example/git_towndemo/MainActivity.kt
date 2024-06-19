24e84dd0b4f4379f1e62a61530bb3aed1b77ee18
Christophe Pelé <97438970+cpele@users.noreply.github.com>
2024-06-19 17:04:39 +0200

    Redirect to 2nd screen

--- app/src/main/java/com/example/git_towndemo/MainActivity.kt
+++ app/src/main/java/com/example/git_towndemo/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.git_towndemo
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,40 +11,54 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.git_towndemo.ui.theme.GitTownDemoTheme
+
+enum class Screen {
+    Main, Secondary
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            var screen by rememberSaveable { mutableStateOf(Screen.Main) }
             GitTownDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Christophe Pelé",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                when (screen) {
+                    Screen.Main -> Greeting(
+                        name = "Christophe Pelé"
+                    ) {
+                        screen = Screen.Secondary
+                    }
+                    Screen.Secondary -> Farewell("Christophe Pelé")
                 }
             }
         }
     }
+
+    @Suppress("SameParameterValue")
+    @Composable
+    private fun Farewell(name: String) {
+        Text("Good bye $name")
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
+fun Greeting(name: String, modifier: Modifier = Modifier, goToSecondary: () -> Unit) {
     Column {
         Text(
             text = "Hello $name!",
             modifier = modifier
         )
         Button(onClick = {
-            Toast.makeText(context, "Let's go", Toast.LENGTH_LONG).show()
+            goToSecondary()
         }) {
-            Text(text = "Go!")
+            Text(text = "Go to 2nd screen!")
         }
     }
 }
@@ -54,6 +67,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GitTownDemoTheme {
-        Greeting("Android")
+        Greeting("Android") {}
     }
 }
