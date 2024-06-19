c332a049556a3d22485f4d3a4d5effeccebfad75
Christophe Pelé <97438970+cpele@users.noreply.github.com>
2024-06-19 16:53:09 +0200

    Add main-screen button

--- app/src/main/java/com/example/git_towndemo/MainActivity.kt
+++ app/src/main/java/com/example/git_towndemo/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.git_towndemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.git_towndemo.ui.theme.GitTownDemoTheme
 
@@ -32,10 +36,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val context = LocalContext.current
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClick = {
+            Toast.makeText(context, "Let's go", Toast.LENGTH_LONG).show()
+        }) {
+            Text(text = "Go!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
